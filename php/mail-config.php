c2b256c180aeb896bfbfb0eba06733f7adbdfa79
Koena Mangena <koena.mangena@standardbank.co.za>
2016-08-15 14:00:51 +0200

    welcome to github

---
+++ php/mail-config.php
@@ -0,0 +1,11 @@
+<?php 
+
+// put your email address here
+define('YOUR_EMAIL_ADDRESS', 'info@phetogogroup.co.za'); 
+define('YOUR_COMPANY_NAME', 'Phetogo Shuttle Services'); 
+
+$mailchimpSupport 	= false; // set 'true' to activate mailchimp support
+
+define('MAILCHIMP_API_KEY', 'b714582d55c439ece1f49cb8d167c4b9-us3');
+define('MAILCHIMP_LIST_ID', 'f84fe4d84c');
+
---
+++ public_html/js/locations-autocomplete.js
@@ -0,0 +1,11 @@
+var locations = [
+
+{ value: 'johannesburg, Gauteng'},
+{ value: 'OR Tambo International Airport'},
+{ value: 'Sandton City'},
+{ value: 'Bryaston'},
+{ value: 'Soweto'},
+{ value: 'Pretoria'},
+{ value: 'Midrand'},
+{ value: 'Lanseria Airport'},
+];
